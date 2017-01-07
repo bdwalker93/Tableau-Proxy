cc6839a6ac6fa18a4afcb366f18558c45e0f5d37
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-01-06 16:35:58 -0800

    stuff

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 var fs = require('fs');
 var PORT = 3000;
-var http = require('http'),
-    httpProxy = require('http-proxy');
+var https = require('https');
+var httpProxy = require('http-proxy');
+var connect = require('connect');
+var harmon = require('harmon');
 
 var proxy = httpProxy.createServer({
-  ssl: {
-    key: fs.readFileSync('localhost.key', 'utf8'),
-    cert: fs.readFileSync('localhost.cert', 'utf8')
-  },
   target: 'https://tableau.ics.uci.edu',
   secure: false
 })
 
-proxy.listen(PORT);
+
+var app = connect();
+
+var selects = [];
+var simpleselect = {};
+
+simpleselect.query = '.tb-login-form-container';
+simpleselect.func = function (node) {
+	node.createWriteStream().end('<div>+ Trumpet</div>');
+}
+
+//selects.push(simpleselect);
+
+app.use(harmon([],selects));
+
+app.use((req, res) => {
+  proxy.web(req, res);
+})
+
+https.createServer({
+  key: fs.readFileSync('localhost.key', 'utf8'),
+  cert: fs.readFileSync('localhost.cert', 'utf8')
+}, app).listen(PORT, function() {
+  console.log('listening on port '+PORT);
+});
