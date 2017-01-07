228b1066e6a1bbf3ee32e1bf6af388b24705e708
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-01-07 03:42:56 +0000

    c00l

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var PORT = 3000;
 var https = require('https');
 var httpProxy = require('http-proxy');
-var connect = require('connect');
+var express = require('express');
 var harmon = require('harmon');
 
 var proxy = httpProxy.createServer({
@@ -10,10 +10,11 @@ var proxy = httpProxy.createServer({
   secure: false
 })
 
-var app = connect();
+var app = express();
 
 var modifications = [];
 
+var style = "<style>"+fs.readFileSync(__dirname+'/inject-style.css')+"</style>";
 
 modifications.push({
   query: 'meta[name=viewport]',
@@ -28,26 +29,38 @@ modifications.push({
   query: 'head',
   func: node => {
 
-    var injectStream = fs.createReadStream(__dirname+'/inject.html');
-    var rs = node.createReadStream();
-    var ws = node.createWriteStream({inner: false});
+		var rs = node.createReadStream();
+		var ws = node.createWriteStream({outer: false});
 
-    // Read the node and put it back into our write stream, 
-    // but don't end the write stream when the readStream is closed.
-    rs.pipe(ws, {end: false});
+		// Read the node and put it back into our write stream, 
+		// but don't end the write stream when the readStream is closed.
+		rs.pipe(ws, {end: false});
 
-    // When the read stream has ended, attach our style to the end
-    rs.on('end', function(){
-      injectStream.pipe(ws);
-    });
+		// When the read stream has ended, attach our style to the end
+		rs.on('end', function(){
+			ws.end(style);
+		});
   }
 });
 
-app.use(harmon([], modifications));
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  if ( req.url === '/' ) {
+    harmon([], modifications)(req, res, next);
+  } else if (req.url.match(/^\/en\/loginSitePicker.html/)) {
+    harmon([], [{
+      query: '.tb-login',
+      func: node => {
+        var rs = fs.createReadStream(__dirname+'/loginSitePicker.html');
+        rs.pipe(node.createWriteStream({outer: true }));
+      }
+    }])(req, res, next);
+  } else {
+    next();
+  }
+}, (req, res, next) => {
   proxy.web(req, res);
-})
+});
 
 https.createServer({
   key: fs.readFileSync('localhost.key', 'utf8'),
