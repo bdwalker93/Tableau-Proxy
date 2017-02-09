e561e7ef698ecef1570f5ade885e0f75c26e7d7d
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-02-08 16:33:13 -0800

    Got popover working... Kind of

--- react/components/VizPage.jsx
+++ react/components/VizPage.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../action-creators';
 import './VizPage.less';
-import { VizHeader} from './VizHeader';
+import { Button, Popover, OverlayTrigger } from 'react-bootstrap';
+import { Link } from 'react-router';
+import './VizHeader.less';
 
 var BzIframe = React.createClass({
 
@@ -22,23 +24,40 @@ var BzIframe = React.createClass({
   }
 });
 
+function popoverContent(views) {
+  return <Popover id="popover-trigger-click-root-close" title="Popover bottom">
+    <ul>
+      {views.map(view=><li key={view.id}>
+        <span>{view.isFavorite ? 'fav' : 'nofav'}</span>
+        <Link to={`/app/workbooks/${view.workbookId}/views/${view.path}`}>{view.name}</Link>
+      </li>)}
+
+    </ul>
+  </Popover>;
+}
+
 const Viz = ({
   site,
   views,
   viewPath,
-}) =>
-  <div className="viz-page">
-    <VizHeader views={views} />
+}) => <div className="viz-page">
+    <div className="viz-header">
+      <Button onClick={()=>window.history.back()}>back</Button>
+      <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={popoverContent(views)}>
+        <Button>Click w/rootClose</Button>
+      </OverlayTrigger>
+    </div>
+
     { site && viewPath ? 
-      <BzIframe onLoad={(iframe)=>{
-        if (!  iframe.contentWindow )  return ;
-        let $ = iframe.contentWindow.jQuery;
-        let t = $('.tableau')[0];
-        let zoom = t.clientWidth / t.scrollWidth * 100;
-        $(t).css({zoom: zoom+'%'});
-      }} src={`/t/${site}/views/${viewPath}?:embed=y&:showVizHome=n&:toolbar=top&:openAuthoringInTopWindow=true&:browserBackButtonUndo=true&:reloadOnCustomViewSave=true&:showShareOptions=true&:size=100,183`}/>
-    : null }
-  </div>
+        <BzIframe onLoad={(iframe)=>{
+          if (!  iframe.contentWindow )  return ;
+          let $ = iframe.contentWindow.jQuery;
+          let t = $('.tableau')[0];
+          let zoom = t.clientWidth / t.scrollWidth * 100;
+          $(t).css({zoom: zoom+'%'});
+        }} src={`/t/${site}/views/${viewPath}?:embed=y&:showVizHome=n&:toolbar=top&:openAuthoringInTopWindow=true&:browserBackButtonUndo=true&:reloadOnCustomViewSave=true&:showShareOptions=true&:size=100,183`}/>
+        : null }
+</div>
 
 function mapStateToProps(state) {
   console.log('STATE', state.viz.views);
